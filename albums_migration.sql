ee92bce70a3e3ea5c8586d3a5ffc069a8837af33
Bernie Esquivel <bernie.esquivel01@gmail.com>
2021-11-17 09:57:41 -0600

    feat: albums_migration.sql exercise complete

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,15 @@
 USE codeup_test_db;
 
+# DROP TABLE IF EXISTS albums;//can do this step to drop table that might have already been created.
+
+# CREATE TABLE IF NOT EXISTS albums();//this will create album if it does not already exist.
+
 CREATE TABLE albums (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                        artist VARCHAR(50) NOT NULL,
+                        artist VARCHAR(50) DEFAULT 'NONE',
                         name  VARCHAR(100) NOT NULL,
-                        release_date INT UNSIGNED NOT NULL,
-                        sales decimal UNSIGNED NOT NULL,
+                        release_date INT,
+                        sales FLOAT,
                         genre VARCHAR(50) NOT NULL,
                         PRIMARY KEY (id)
 );
