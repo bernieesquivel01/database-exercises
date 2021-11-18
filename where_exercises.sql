4bcc6cdb94537fcd228231860868c6c762fa37ad
Bernie Esquivel <bernie.esquivel01@gmail.com>
2021-11-18 15:08:00 -0600

    feat: where_exercises.sql complete

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,13 +3,13 @@
 USE employees;
 
 # Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
-# SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
-#
-# # Find all employees whose last name starts with 'E' — 7,330 rows.
-# SELECT * FROM employees WHERE last_name LIKE 'E%';
-#
-# # Find all employees with a 'q' in their last name — 1,873 rows.
-# SELECT * FROM employees WHERE last_name LIKE '%q%';
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+# Find all employees whose last name starts with 'E' — 7,330 rows.
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+
+# Find all employees with a 'q' in their last name — 1,873 rows.
+SELECT * FROM employees WHERE last_name LIKE '%q%';
 
 # PART TWO
 # Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
